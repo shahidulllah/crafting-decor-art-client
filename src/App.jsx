35d60ea3391c732ba1bc8299bc343fcca34b5a58
Md. Shahidullah <155251926+shahidulllah@users.noreply.github.com>
2025-09-24 16:13:30 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
+      <h1 className="text-3xl text-center font-bold">
       Hello, this is new journey
     </h1>
     </>
